1a33db4465c3fb72a273f0c0f6c060d15a283edd
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-19 05:33:55 +0200

    private route added

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Room = ({ room }) => {
-  const { bed, picture, price, type } = room;
+  const { bed, picture, price, type, _id } = room;
+  const navigate = useNavigate();
+
+  const handleBook = () => {
+    navigate(`room/${_id}`);
+  };
 
   return (
-    <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+    <div className="overflow-hidden transition-shadow duration-300 bg-white shadow-2xl p-5 rounded rounded-2xl">
       <a href="/" aria-label="Article">
         <img
           src={picture}
@@ -24,72 +30,12 @@ const Room = ({ room }) => {
           <p className="text-2xl font-bold leading-5">{type}</p>
         </a>
         <p className="mb-4 text-gray-700 font-semibold">Bed: {bed}</p>
-        <div className="flex space-x-4">
-          <a
-            href="/"
-            aria-label="Likes"
-            className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-          >
-            <div className="mr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-              >
-                <polyline
-                  points="6 23 1 23 1 12 6 12"
-                  fill="none"
-                  strokeMiterlimit="10"
-                />
-                <path
-                  d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                />
-              </svg>
-            </div>
-            <p className="font-semibold">7.4K</p>
-          </a>
-          <a
-            href="/"
-            aria-label="Comments"
-            className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-          >
-            <div className="mr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-              >
-                <polyline
-                  points="23 5 23 18 19 18 19 22 13 18 12 18"
-                  fill="none"
-                  strokeMiterlimit="10"
-                />
-                <polygon
-                  points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                />
-              </svg>
-            </div>
-            <p className="font-semibold">81</p>
-          </a>
-        </div>
+        <button
+          onClick={handleBook}
+          className="btn btn-outline bg-sky-100 w-1/2"
+        >
+          Book
+        </button>
       </div>
     </div>
   );
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../Pages/Home/Home/Home";
+import RoomDetail from "../Pages/RoomDetail/RoomDetail/RoomDetail";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,12 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/room/:roomId",
+        element: <RoomDetail />,
+        loader: ({ params: { roomId } }) =>
+          fetch(`http://localhost:15000/rooms/${roomId}`),
       },
     ],
   },
