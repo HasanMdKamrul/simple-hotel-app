d8617ed7a50a6908d4524581d93b8dc1e6a13e47
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-19 02:52:08 +0200

    Initial project setup and dev dependencies added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRrJrMyCS0C71GWdbSTr5gh_XOScFi5Ik",
+  authDomain: "simple-hotel-app-d7b1b.firebaseapp.com",
+  projectId: "simple-hotel-app-d7b1b",
+  storageBucket: "simple-hotel-app-d7b1b.appspot.com",
+  messagingSenderId: "1080775842439",
+  appId: "1:1080775842439:web:cbb1d6d7b72d8861fbb622",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
